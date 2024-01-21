56a38b86ef5a49ce5a1d8351ebb2963b0b25da00
Mário Chaves <mariochaves@MacBook-Pro-de-Mario.local>
2024-01-20 23:52:45 -0300

    Console application created

---
+++ ccwc/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Input your value:");
+var asd = Console.ReadLine();
+Console.WriteLine($"Hello, World you have input {asd}");
